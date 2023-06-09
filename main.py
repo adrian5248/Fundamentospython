7e80ac48d8304622d4121afdf517084f6dcda5b7
Adrian <adrian.duran.matias@gmail.com>
2023-06-09 16:20:38 -0600

    01 Funcion Print

---
+++ main.py
@@ -0,0 +1,9 @@
+# Function print 
+print("Hola Mundo")
+print("Soy Alumno de TIADSM")
+
+print("Podemos enviar diferentes tipos de auto")
+print(12)
+print(12.23)
+print(True)
+print([])
---
+++ variables.py
@@ -0,0 +1,14 @@
+name = "Adrian"
+lastname = "Duran"
+age = 18
+height = 1.73
+is_student = True
+groups = ["3A", "3B", "3C"]
+
+
+# function type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
