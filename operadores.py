4947d874d35fb517849ee3f7b303be53cbc9382f
Adrian <adrian.duran.matias@gmail.com>
2023-06-13 12:10:05 -0600

    04 operadores logicos

---
+++ operadores.py
@@ -0,0 +1,19 @@
+# operadores matematicos
+# + - * %
+# / Division float
+# // Division entera
+# ** Potencia
+
+print(12 * 12)
+print(12 - 12)
+print(12 * 12)
+print(12 % 12)
+print(10 / 3)
+print(10 // 3)
+print(2 ** 16)
+# operadores para los strings
+# + para concatenar
+# * para replicar 
+print("___" * 4)
+print("hola " + "mundo")
+print("hola " * 3)
---
+++ operadores_2.py
@@ -0,0 +1,18 @@
+# operadores boleanos 
+# ==(igualdad)
+print(1 == 1)
+# != (Distinto)
+print(1!= 1)
+# > (mayor que)
+print(1 > 1)
+# < (menor que)
+print(1 < 1)
+# operadores logicos
+# and 
+print(True and True)
+print(True and False)
+# or 
+print(True or True)
+print(True or False)
+# not
+print(not True)
